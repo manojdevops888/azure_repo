19d3aeed05bc055f7dbb67049c4c675572e57255
manoj <manoj.india888@gmail.com>
2025-08-16 16:56:47 +0530

    new rg added

--- todo_infra/main.tf
+++ todo_infra/main.tf
@@ -5,5 +5,9 @@ module "resource_group" {
   rg_details_name = "james-rg"
   rg_details_location = "westus"
 }
-
+module "resource_group" {
+  source = "../MODULE/RESOURCE_GROUP"
+  rg_details_name = "james-rg1"
+  rg_details_location = "westus"
+}
 
